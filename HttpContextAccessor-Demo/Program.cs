7a66f4aa852813eab90a1fe83c8bae9de64964b8
DESKTOP\safas <g.safalidis@gmail.com>
2025-03-14 13:32:42 +0200

    Use extension method to initialize HttpContextAccessor

--- HttpContextAccessor-Demo/Program.cs
+++ HttpContextAccessor-Demo/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddLogging();
 
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddScoped<PaymentHandler>();
 
